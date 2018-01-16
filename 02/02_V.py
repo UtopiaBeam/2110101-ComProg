21226fecb8d897ba3ae2eaab0891917ab4c87fc0
UtopiaBeam <utopiabeam@gmail.com>
2018-01-16 12:16:29 +0700

    Solutions for 02

---
+++ 02/02_V.py
@@ -0,0 +1,88 @@
+# 02_V1 (Simple)
+answers = [ 'a', 'e', 'e', 'b', 'c' ]
+n = int(input())
+print(answers[n-1].lower())
+
+# 02_V1 (Advance)
+print('aeebc'[int(input())-1])
+
+
+# 02_V2 (Simple)
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if a > b :
+    a, b = b, a
+if b > c :
+    b, c = c, b
+if c > d :
+    c, d = d, c
+if a > b :
+    a, b = b, a
+if b > c :
+    b, c = c, b
+if a > b :
+    a, b = b, a
+print(a, b, c, d)
+
+
+# 02_V2 (Advance)
+print(*sorted([int(input()) for _ in range(4)]))
+
+# 02_V3 (Simple)
+d = float(input())
+max = d
+min = d
+d = float(input())
+if d > max :
+    max = d
+if d < min :
+    min = d
+d = float(input())
+if d > max :
+    max = d
+if d < min :
+    min = d
+d = float(input())
+if d > max :
+    max = d
+if d < min :
+    min = d
+d = float(input())
+if d > max :
+    max = d
+if d < min :
+    min = d
+d = float(input())
+if d > max :
+    max = d
+if d < min :
+    min = d
+print(min, max)
+
+# 02_V3 (Advance: using loop)
+mn = float('inf')
+mx = float('-inf')
+for i in range(0, 6) :
+    x = float(input())
+    mn = min(mn, x)
+    mx = max(mx, x)
+print(mn, mx)
+
+# 02_V3 (Advance: using list + min & max functions)
+ls = [float(input()) for _ in range(6)]
+print(min(ls), max(ls))
+
+# 02_V4 (Advance)
+day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+d = int(input())
+m = int(input())
+y = int(input()) - 543
+for i in range(m-1) :
+    d += day[i]
+# Leap year check
+if m > 1 and (y % 400 == 0 or (y % 4 == 0 and y % 100 > 0)) :
+    d += 1
+print(d)
+
