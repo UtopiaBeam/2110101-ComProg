660224640867f546b9c5fe5969b1331a2919317a
UtopiaBeam <utopiabeam@gmail.com>
2018-02-01 13:41:38 +0700

    Solutions for 04

--- 04/04_P.py
+++ 04/04_P.py
@@ -150,3 +150,119 @@ for c in input().strip() :
     dc[c] += 1
     ls.append((c, dc[c]))
 print(*(c for c, x in ls if dc[c] == 1 or x == 2), sep = '')
+
+# 04_P16 (Adv: function)
+num = ['soon', 'nueng', 'song', 'sam', 'see', 'ha', 'hok', 'jed', 'pad', 'kao']
+dig = ['', '', 'sip', 'roey', 'pun', 'muen', 'saen']
+sp = [('nueng-sip', 'sip'), ('sip-nueng', 'sip-ed'), ('song-sip', 'yee-sip')]
+n, d, *_ = tuple([*input().strip().replace(',', '').split('.'), ''])
+ls = []
+def th(n) :
+    while len(n) > 0 :
+        if n[0] in '123456789' :
+            ls.append(num[int(n[0])])
+            if len(n) > 1 :     ls.append(dig[len(n)])
+        n = n[1:]
+if '-' in n :
+    print('lop-', end = '')
+    n = n[1:]
+if n == '0' :
+    ls.append('soon')
+    n = n[1:]
+while len(n) > 6 :
+    th(n[:-6])
+    n = n[-6:]
+    ls.append('larn')
+th(n)
+if d :      ls.append('jood')
+for x in d :
+    ls.append(num[int(x)])
+ans = '-'.join(ls)
+for x, y in sp :
+    ans = ans.replace(x, y)
+print(ans)
+
+# 04_P17 (Adv: reduce() + zip() + dict + function)
+from functools import reduce
+num = dict(zip(['soon', 'nueng', 'song', 'sam', 'see', 'ha', 'hok', 'jed', 'pad', 'kao'], range(10)))
+dig = dict(zip(['sip', 'roey', 'pun', 'muen', 'saen'], [10**x for x in range(1, 7)]))
+sp = [('yee-sip', 'song-sip'), ('sip-ed', 'sip-nueng')]
+n, d, *_ = (*input().strip().split('-jood-'), '')
+n = reduce(lambda a, b : a.replace(*b), sp, n).split('-')
+ans, tmp, neg = 0, 0, False
+for c in n :
+    if c == 'lop' :     neg = True
+    elif c in num :     tmp += num[c]
+    elif c == 'larn' :
+        ans, tmp = (ans+tmp) * 10**6, 0
+    else :
+        if c == 'sip' and tmp == 0 :
+            tmp = 10
+        else :
+            ans += tmp * dig[c]
+            tmp = 0
+print(format(-ans-tmp if neg else ans+tmp, ',d'), end = '')
+if d :  print('.', *[num[x] for x in d.split('-')], sep = '')
+
+# 04_P18
+n = int(input())
+ls = [input().strip() for _ in range(n)]
+pre = suf = ls[0]
+for s in ls :
+    tmp = ''
+    for i in range(min(len(pre), len(s))) :
+        if s[i] != pre[i] :     break
+        tmp += s[i]
+    pre, tmp = tmp, ''
+    for i in range(-1, -1-min(len(suf), len(s)), -1) :
+        if s[i] != suf[i] :     break
+        tmp = s[i] + tmp
+    suf, tmp = tmp, ''
+print(pre or 'NO COMMON PREFIX')
+print(suf or 'NO COMMON SUFFIX')
+
+# 04_P18 (Adv: os.path.commonprefix())
+from os.path import commonprefix
+n = int(input())
+ls = [input().strip() for _ in range(n)]
+print(commonprefix(ls) or 'NO COMMON PREFIX')
+ls = [s[::-1] for s in ls]
+print(commonprefix(ls)[::-1] or 'NO COMMON SUFFIX')
+
+# 04_P19
+a, b = input().strip(), input().strip()
+ans = []
+for c in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' :
+    if c in a and c in b :
+        ans.append(c)
+print(' '.join(ans) if ans else 'NONE')
+
+# 04_P19 (Adv: set + sorted())
+print(' '.join(sorted(set(input().strip()) & set(input().strip()), key = str.swapcase))  or 'NONE')
+
+# 04_P20 (Adv: dict + function)
+x, y = 0, 0
+dc = {
+    'U' : lambda x, y : (x, y+1),
+    'D' : lambda x, y : (x, max(0, y-1)),
+    'L' : lambda x, y : (max(0, x-1), y),
+    'R' : lambda x, y : (x+1, y)
+}
+for c in input().strip() :
+    x, y = dc[c](x, y)
+print('({},{})'.format(x, y))
+
+# 04_P21
+a, b = input().strip(), input().strip()
+if a in b :     print('SUBSTRING')
+else :
+    while a and b :
+        if a[0] == b[0] :
+            a = a[1:]
+        b = b[1:]
+    print('NONE' if a else 'SUBSEQUENCE')
+
+# 04_P21 (Adv: iter())
+a, b = input().strip(), input().strip()
+ia, ib = iter(a), iter(b)
+print(['NONE', 'SUBSEQUENCE', 'SUBSTRING'][(a in b) + all(any(ca == cb for cb in ib) for ca in ia)])
