b4d91313e5d6ec59e005d61ad85af4c71351d49b
UtopiaBeam <utopiabeam@gmail.com>
2018-02-10 17:45:54 +0700

    Solutions for
    Solutions for Old quizes

--- Old_Q/59_2_Q1.py
+++ Old_Q/59_2_Q1.py
@@ -17,3 +17,13 @@ elif a != 0 :
 elif b != 0 :       print('{:.2f}'.format(-c/b))
 elif c != 0 :       print('No roots exists')
 else :              print('Roots are any numbers')
+
+# 59_2_Q1_4_p
+from math import sin, pi
+t, mn = 0, [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+d1, m1, y1, d2, m2, y2 = (int(x) for x in input().split())
+y1, y2 = y1-543, y2-543
+t += mn[m1] - d1 + sum(mn[m1+1:]) + (1 if m1 <= 2 and (y1 % 400 == 0 or (y1 % 4 == 0 and y1 % 100 != 0)) else 0)
+t += d2 + sum(mn[:m2]) + (1 if m2 > 2 and (y2 % 400 == 0 or (y2 % 4 == 0 and y2 % 100 != 0)) else 0)
+t += 365 * max(0, y2-y1-1)
+print(t, '{:.2f} {:.2f} {:.2f}'.format(sin(2*pi*t/23), sin(2*pi*t/28), sin(2*pi*t/33)))
