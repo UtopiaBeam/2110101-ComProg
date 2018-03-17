328cc20ccc2848f27d8b688e27d1d3f3cf2ae146
UtopiaBeam <utopiabeam@gmail.com>
2018-03-17 08:56:10 +0700

    Solutions for 08

--- 08/08_P.py
+++ 08/08_P.py
@@ -39,23 +39,24 @@ print(sum(dc[x]*dc[n-x] if x != n-x else dc[x]*(dc[x]-1) for x in ls) // 2)
 dc, n = __import__('collections').Counter(list(map(int, input().split()))), int(input())
 print(sum(dc[x]*dc[n-x] if x != n-x else dc[x]*(dc[x]-1) for x in dc) // 2)
 
-# 08_P6 (Issue: TLE)
-_ = input()
-ls, qs = (list(map(int, input().split())) for _ in range(2))
-for q in qs :
-    flag = False
-    for i in range(len(ls)-2) :
-        l, r = i+1, len(ls)-1
-        while l < r :
-            if ls[i] + ls[l] + ls[r] == q :
-                flag = True
-                # print(ls[i], ls[l], ls[r])
-                break
-            if ls[i] + ls[l] + ls[r] < q :
-                l += 1
-            else :      r -= 1
-        if flag :       break
-    print('YES' if flag else 'NO')
+# 08_P6
+n, m = map(int, input().split())
+ls = list(map(int, input().split()))
+dc, ans = {}, [False]*m
+
+for i, x in enumerate(map(int, input().split())) :
+    for j in range(n) :
+        if x-ls[j] not in dc :  dc[x-ls[j]] = []
+        dc[x-ls[j]].append((j, i))
+
+for i in range(n) :
+    for j in range(i+1, n) :
+        sm = ls[i] + ls[j]
+        if sm not in dc :   continue
+        for k, idx in dc[sm] :
+            if i != k and j != k :
+                ans[idx] = True
+print(*['YES' if flag else 'NO' for flag in ans], sep = '\n')            
 
 # 08_P7 (Adv: defaultdict + lambda)
 ls = [input().strip().split()[-1] for _ in range(int(input()))]
